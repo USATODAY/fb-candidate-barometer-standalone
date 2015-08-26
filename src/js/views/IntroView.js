d8159406f8206102213b26f813aa35d3b8f61ae7
mitchthorson <mthorson@usatoday.com>
2015-08-26 14:06:20 -0400

    add skip to intro

--- src/js/views/IntroView.js
+++ src/js/views/IntroView.js
@@ -10,8 +10,8 @@ define([
         initialize: function() {
             this.addTouchEvents();
             this.listenTo(Backbone, "router:home", this.show);
-            this.listenTo(Backbone, "router:candidate", this.advance);
-            this.listenTo(Backbone, "router:candidateSelect", this.advance);
+            this.listenTo(Backbone, "router:candidate", this.skip);
+            this.listenTo(Backbone, "router:candidateSelect", this.skip);
         },
         addTouchEvents: function() {
             this.ht = new Hammer(this.el);
@@ -25,6 +25,9 @@ define([
         },
         advance: function(e) {
             this.$el.velocity({"translateY": "-100%"}, {duration: 400, easing: "swing"});
+        },
+        skip: function(e) {
+            this.$el.velocity({"translateY": "-100%"}, {duration: 0});
         },
         show: function() {
             this.$el.velocity({"translateY": "0%"}, {duration: 400, easing: "swing"});
